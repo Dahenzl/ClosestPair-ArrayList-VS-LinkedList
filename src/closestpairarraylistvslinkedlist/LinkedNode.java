6066bd180e18fae150363c0d7c7ebc1f6fb1cb9a
Dahenzl <ddhenriquez@uninorte.edu.co>
2022-11-25 15:38:43 -0500

    Linked List implemented.

---
+++ src/closestpairarraylistvslinkedlist/LinkedNode.java
@@ -0,0 +1,18 @@
+
+package closestpairarraylistvslinkedlist;
+
+/**
+ *
+ * @author ddhenriquez
+ */
+public final class LinkedNode {
+    final int x;
+    final int y;
+    LinkedNode next;
+    
+    public LinkedNode(int x, int y){
+        this.x = x;
+        this.y = y;
+        next = null;
+    }
+}
